3493733b9d1814ae21860793c1ce9c246cefd649
uploader4tv <88353584+uploader4tv@users.noreply.github.com>
2021-11-22 01:34:20 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -17,7 +17,7 @@ from info import ADMINS
 async def addfilter(client, message):
     userid = message.from_user.id if message.from_user else None
     if not userid:
-        return await message.reply(f"You are anonymous admin. Use /connect {message.chat.id} in PM")
+        return await message.reply(f"Nigglet you're not an admin. Use /connect {message.chat.id} in PM")
     chat_type = message.chat.type
     args = message.text.html.split(None, 1)
 
@@ -29,10 +29,10 @@ async def addfilter(client, message):
                 chat = await client.get_chat(grpid)
                 title = chat.title
             except:
-                await message.reply_text("Make sure I'm present in your group!!", quote=True)
+                await message.reply_text("Uh this ain't allowed", quote=True)
                 return
         else:
-            await message.reply_text("I'm not connected to any groups!", quote=True)
+            await message.reply_text("TF you doing? I'll call uploader ;-;", quote=True)
             return
 
     elif chat_type in ["group", "supergroup"]:
