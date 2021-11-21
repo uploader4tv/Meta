143858f5539d3c9c383ce62ebff0315cba223fe8
uploader4tv <88353584+uploader4tv@users.noreply.github.com>
2021-11-22 01:51:49 +0530

    Update connection.py

--- plugins/connection.py
+++ plugins/connection.py
@@ -7,7 +7,7 @@ from info import ADMINS
 async def addconnection(client,message):
     userid = message.reply_to_message.from_user.id if message.from_user else None
     if not userid:
-        return await message.reply(f"You are anonymous admin. Use /connect {message.chat.id} in PM")
+        return await message.reply(f"You are not an admin. Use /connect {message.chat.id} in PM")
     chat_type = message.chat.type
 
     if chat_type == "private":
@@ -15,9 +15,9 @@ async def addconnection(client,message):
             cmd, group_id = message.text.split(" ", 1)
         except:
             await message.reply_text(
-                "<b>Enter in correct format!</b>\n\n"
+                "<b>yo maintain the format ffs</b>\n\n"
                 "<code>/connect groupid</code>\n\n"
-                "<i>Get your Group id by adding this bot to your group and use  <code>/id</code></i>",
+                "<i>don't know how to get your group's ID? pch send /id after adding me to your group<code>/id</code></i>",
                 quote=True
             )
             return
@@ -32,12 +32,12 @@ async def addconnection(client,message):
             and st.status != "creator"
             and str(userid) not in ADMINS
         ):
-            await message.reply_text("You should be an admin in Given group!", quote=True)
+            await message.reply_text("You are not even an admin there lmao, noob", quote=True)
             return
     except Exception as e:
         print(e)
         await message.reply_text(
-            "Invalid Group ID!\n\nIf correct, Make sure I'm present in your group!!",
+            "Either the group ID is fucked or I am not a particpant there, fix it bitch",
             quote=True,
         )
 
@@ -51,7 +51,7 @@ async def addconnection(client,message):
             addcon = await add_connection(str(group_id), str(userid))
             if addcon:
                 await message.reply_text(
-                    f"Sucessfully connected to **{title}**\nNow manage your group from my pm !",
+                    f"Sucessfully connected to **{title}**",
                     quote=True,
                     parse_mode="md"
                 )
@@ -70,7 +70,7 @@ async def addconnection(client,message):
             await message.reply_text("Add me as an admin in group", quote=True)
     except Exception as e:
         print(e)
-        await message.reply_text('Some error occured! Try again later.', quote=True)
+        await message.reply_text('System is fucked ggwp', quote=True)
         return
 
 
@@ -78,11 +78,11 @@ async def addconnection(client,message):
 async def deleteconnection(client,message):
     userid = message.reply_to_message.from_user.id if message.from_user else None
     if not userid:
-        return await message.reply(f"You are anonymous admin. Use /connect {message.chat.id} in PM")
+        return await message.reply(f"You are not an admin. Use /connect {message.chat.id} in PM")
     chat_type = message.chat.type
 
     if chat_type == "private":
-        await message.reply_text("Run /connections to view or disconnect from groups!", quote=True)
+        await message.reply_text("Run /connections I guess , hehe", quote=True)
 
     elif chat_type in ["group", "supergroup"]:
         group_id = message.chat.id
@@ -99,7 +99,7 @@ async def deleteconnection(client,message):
         if delcon:
             await message.reply_text("Successfully disconnected from this chat", quote=True)
         else:
-            await message.reply_text("This chat isn't connected to me!\nDo /connect to connect.", quote=True)
+            await message.reply_text("yo how do I disconnect when I never connected ; stupidfuck", quote=True)
 
 
 @Client.on_message(filters.private & filters.command(["connections"]))
